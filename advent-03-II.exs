f1f3dbe02ba70af17c4bbba4a81f2467e4f73d22
Johannes Opper <johannes.opper@gmail.com>
2016-12-03 22:05:42 +0100

    Correct indentation

--- advent-03-II.exs
+++ advent-03-II.exs
@@ -7,12 +7,12 @@ end
 transpose = fn(m)-> List.zip(m) |> Enum.map(&Tuple.to_list(&1)) end
 
 IO.inspect File.read!("advent-03-input.txt")
-  |> String.split                             # Split whole file by whitespace
+  |> String.split                         # Split whole file by whitespace
   |> Enum.map(&String.to_integer/1)       # Convert all to integer
-  |> Enum.chunk(3)                            # Get original lines
-  |> Enum.chunk(3)                            # Group lines by 3
-  |> Enum.map(&(transpose.(&1)))              # Transpose rows and cols
-  |> List.flatten                             # Flatten for..
-  |> Enum.chunk(3)                            #              ..regrouping
+  |> Enum.chunk(3)                        # Get original lines
+  |> Enum.chunk(3)                        # Group lines by 3
+  |> Enum.map(&(transpose.(&1)))          # Transpose rows and cols
+  |> List.flatten                         # Flatten for..
+  |> Enum.chunk(3)                        #              ..regrouping
   |> Enum.filter(&Triangle.is_valid/1)    # Throw invalid lines out
-  |> Enum.count                               # Count the rest
+  |> Enum.count                           # Count the rest
